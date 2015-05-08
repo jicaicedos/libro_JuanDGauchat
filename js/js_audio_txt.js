46a9c80a8ee169e44bd46d0d4513c72fd0ad1dff
Javier Caicedo <jicaicedocpe@gmail.com>
2015-05-08 10:47:53 -0500

    COD: Terminado estilos para pagina de video

--- js/js_audio_txt.js
+++ js/js_audio_txt.js
@@ -2,17 +2,16 @@
 	function() {
 		text = document.getElementById("texto_codigo")
 		texto_codigo.innerHTML = "// Código HTML<br /><br />"
-		texto_codigo.innerHTML += "<strong>< audio id='miaudio' width='200' ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< source src='audios/Himno de Colombia.mp3' ></source ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< source src='audios/Himno de Colombia.ogg' ></source ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< /audio ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< div id='reproductor_audio_barra' ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< div id='progreso_audio' ></div ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< /div ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< /nav ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< p id='reproductor_audio_avance' >0:0:0< /p ></strong><br />"
-		texto_codigo.innerHTML += "<strong>< p id='reproductor_audio_duracion' >0:0:0< /p ></strong><br>"
-		texto_codigo.innerHTML += "<strong></strong><br>"
+		texto_codigo.innerHTML += "< audio id='miaudio' width='200' ><br />"
+		texto_codigo.innerHTML += "< source src='audios/Himno de Colombia.mp3' ></source ><br />"
+		texto_codigo.innerHTML += "< source src='audios/Himno de Colombia.ogg' ></source ><br />"
+		texto_codigo.innerHTML += "< /audio ><br />"
+		texto_codigo.innerHTML += "< div id='reproductor_audio_barra' ><br />"
+		texto_codigo.innerHTML += "< div id='progreso_audio' ></div ><br />"
+		texto_codigo.innerHTML += "< /div ><br />"
+		texto_codigo.innerHTML += "< p id='reproductor_audio_avance' >0:0:0< /p ><br />"
+		texto_codigo.innerHTML += "< p id='reproductor_audio_duracion' >0:0:0< /p ><br>"
+		texto_codigo.innerHTML += "<br>"
 		texto_codigo.innerHTML += "// Código JavaScript del reproductor de audio<br>"
 	}
 )();
